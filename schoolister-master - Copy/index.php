c8cb6626b82d5e665ae4e4be50c3b8ffbb35f990
Noderz <58016133+Noderz@users.noreply.github.com>
2020-01-25 12:17:10 -0800

    Add files via upload

--- schoolister-master - Copy/index.php
+++ schoolister-master - Copy/index.php
@@ -1,33 +1,132 @@
-<!DOCTYPE html>
 <html>
-<head>
-<title>My Drive - Google Drive</title>
-<link rel="icon" href="https://raw.githubusercontent.com/Noderz/schoolister/master/download%20(3).ico">
-</head>
-<body style="background-color:powderblue;">
+	<head>
+			<title>My Drive - Google Drive</title>
+			<link rel="icon" href="https://ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_4.ico">
+	</head>
+</html>
+<?php
 
-<center>
-<h1 style="font-family:comic sans ms;background-color:tomato"><ins>Schoolist3r</ins></h1>
-<br>
-<img src="nlogo.png" width = 300 height= 300 alt= logo>
-</br>
-<br>
-<ins>
-<i>
-<h2 style="font-family:Agency FB;">Made For Students, By Students.</h2>
-</i>
-</ins>
-<p style="font-family:Calibri;background-color:tomato"> Before using, make sure to put your browser in private/incognito mode. <ins>If you do not put in that mode, the site will be blocked.</ins> 
-<br> Please do me a favor on doing that. It really helps me keep this site running. Thank you! -Noderz
-<br>
-<br>
-<b><i><u>Last Updated 1/7/20.</u></i></b>
-</p>
-</style>
-</br>
-</body>
-</br>
-<a href="games.html"><button style="color:white;font-family:comic sans ms;background-color:DodgerBlue;height:200px;width:300px"><ins>Arcade</ins></button></a><a href="proxselection.html"><button style="color:white;font-family:comic sans ms;background-color:DodgerBlue;height:200px;width:300px"><ins>Surf Freely</ins></button></a><a href="tools.html"><button style="color:white;font-family:comic sans ms;background-color:DodgerBlue;height:200px;width:300px"><ins>Useful Tools</ins></button></a>
-<br>
-<img src="download.gif" width= 1000 height= 562 alt= gif />
-</br>
+define('PROXY_START', microtime(true));
+
+require("vendor/autoload.php");
+
+use Proxy\Http\Request;
+use Proxy\Http\Response;
+use Proxy\Plugin\AbstractPlugin;
+use Proxy\Event\FilterEvent;
+use Proxy\Config;
+use Proxy\Proxy;
+
+// start the session
+session_start();
+
+// load config...
+Config::load('./config.php');
+
+// custom config file to be written to by a bash script or something
+Config::load('./custom_config.php');
+
+if(!Config::get('app_key')){
+	die("app_key inside config.php cannot be empty!");
+}
+
+if(!function_exists('curl_version')){
+	die("cURL extension is not loaded!");
+}
+
+// how are our URLs be generated from this point? this must be set here so the proxify_url function below can make use of it
+if(Config::get('url_mode') == 2){
+	Config::set('encryption_key', md5(Config::get('app_key').$_SERVER['REMOTE_ADDR']));
+} else if(Config::get('url_mode') == 3){
+	Config::set('encryption_key', md5(Config::get('app_key').session_id()));
+}
+
+// very important!!! otherwise requests are queued while waiting for session file to be unlocked
+session_write_close();
+
+// form submit in progress...
+if(isset($_POST['url'])){
+
+	$url = $_POST['url'];
+	$url = add_http($url);
+
+	header("HTTP/1.1 302 Found");
+	header('Location: '.proxify_url($url));
+	exit;
+
+} else if(!isset($_GET['q'])){
+
+	// must be at homepage - should we redirect somewhere else?
+	if(Config::get('index_redirect')){
+
+		// redirect to...
+		header("HTTP/1.1 302 Found");
+		header("Location: ".Config::get('index_redirect'));
+
+	} else {
+		echo render_template("./templates/main.php", array('version' => Proxy::VERSION));
+	}
+
+	exit;
+}
+
+// decode q parameter to get the real URL
+$url = url_decrypt($_GET['q']);
+
+$proxy = new Proxy();
+
+// load plugins
+foreach(Config::get('plugins', array()) as $plugin){
+
+	$plugin_class = $plugin.'Plugin';
+
+	if(file_exists('./plugins/'.$plugin_class.'.php')){
+
+		// use user plugin from /plugins/
+		require_once('./plugins/'.$plugin_class.'.php');
+
+	} else if(class_exists('\\Proxy\\Plugin\\'.$plugin_class)){
+
+		// does the native plugin from php-proxy package with such name exist?
+		$plugin_class = '\\Proxy\\Plugin\\'.$plugin_class;
+	}
+
+	// otherwise plugin_class better be loaded already through composer.json and match namespace exactly \\Vendor\\Plugin\\SuperPlugin
+	$proxy->getEventDispatcher()->addSubscriber(new $plugin_class());
+}
+
+try {
+
+	// request sent to index.php
+	$request = Request::createFromGlobals();
+
+	// remove all GET parameters such as ?q=
+	$request->get->clear();
+
+	// forward it to some other URL
+	$response = $proxy->forward($request, $url);
+
+	// if that was a streaming response, then everything was already sent and script will be killed before it even reaches this line
+	$response->send();
+
+} catch (Exception $ex){
+
+	// if the site is on server2.proxy.com then you may wish to redirect it back to proxy.com
+	if(Config::get("error_redirect")){
+
+		$url = render_string(Config::get("error_redirect"), array(
+			'error_msg' => rawurlencode($ex->getMessage())
+		));
+
+		// Cannot modify header information - headers already sent
+		header("HTTP/1.1 302 Found");
+		header("Location: {$url}");
+
+	} else {
+
+		header("Location: local3.html");
+
+	}
+}
+
+?>
